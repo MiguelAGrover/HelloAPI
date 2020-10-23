bc8a9fbaacbf313796029977745da575d9b7bfad
MiguelAGrover <50057779+MiguelAGrover@users.noreply.github.com>
2020-10-22 19:33:01 -0500

    Added validations and test for weather API

--- routes/apiClient.js
+++ routes/apiClient.js
@@ -11,21 +11,34 @@ let routeBuilder = path => {
     });
 
     router.get(`${path}/weather`, (req,res) => {
-        console.log(req.query.longitud)
-        var longitud = (req.query.longitud) ? req.query.longitud : 113.17;
-        var latitud = (req.query.latitud) ? req.query.latitud : 23.09;
+        var longitud = Number((req.query.longitud) ? req.query.longitud : 113.17);
+        var latitud = Number((req.query.latitud) ? req.query.latitud : 23.09);
         var product = (req.query.product) ? req.query.product : "civil";
         var output = (req.query.output) ? req.query.output : "json";
-
+        var productList = ['civil', 'astro', 'civillight', 'meteo','two'];
+        var outputList = ['json', 'xml']
+        if(Number.isNaN(longitud)){
+            {res.status(404).json({'ERR':'Longitud was not a number'})};
+        }
+        else if(Number.isNaN(latitud)){
+            {res.status(404).json({'ERR':'Latitud was not a number'})};
+        }
+        else if(typeof product != 'string' || !productList.includes(product)){
+            {res.status(404).json({'ERR':'Product was not one of the following values [civil, astro, civillight, meteo or two]'})};
+        }
+        else if(typeof output != 'string' || !outputList.includes(output)){
+            {res.status(404).json({'ERR':'Output was not one of the following values[json, xml]'})};
+        } else {
         result = getWeatherAPI(longitud,latitud,product,output)
                 .then(data => {
                     res.json(data)
                 })
                 .catch(err => {res.status(404).json(err)})
+        }
     });
 
     router.get(`${path}/nationalize`, (req,res) => {
-        name = req.query.name;
+        var name = (req.query.name) ? req.query.name : "wizeline";
         result = getNationalizeAPI(name)
                 .then(data => {
                     res.json(data)
--- tests/testWeatherAPI.js
+++ tests/testWeatherAPI.js
@@ -5,44 +5,74 @@ const app = require('../index');
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
-describe("Weather API", () => {
-    describe("GET /weather", () => {
+describe("Weather API", function() {
+    describe("GET /weather", function() {
         // Test to get homep page
-        it("should get weather information", (done) => {
+        this.timeout(10000);
+        it("should get weather information", function(done) {
              chai.request(app)
-                 .get('/weather')
+                 .get('/apiClient/weather')
                  .end((err, res) => {
                      res.should.have.status(200);
                      res.body.should.be.a('object');
+                     res.body.should.have.property('dataseries');
                      done();
                   });
          });
 
-        /*
-
         // Test to get single student record
-        it("should get a single student record", (done) => {
-             const id = 1;
+        it("should get weather information with two parameters", (done) => {
+             const longitud = 100;
+             const latitud = 20;
              chai.request(app)
-                 .get(`/${id}`)
+                 .get(`/apiClient/weather?longitud=${longitud}&latitud=${latitud}`)
                  .end((err, res) => {
                      res.should.have.status(200);
                      res.body.should.be.a('object');
+                     res.body.should.have.property('dataseries');
                      done();
                   });
          });
          
         // Test to get single student record
-        it("should not get a single student record", (done) => {
-             const id = 5;
-             chai.request(app)
-                 .get(`/${id}`)
-                 .end((err, res) => {
-                     res.should.have.status(404);
-                     done();
-                  });
+        it("should get error of invalid coordinate", (done) => {
+            const longitud = 200;
+            const latitud = 200;
+            chai.request(app)
+                .get(`/apiClient/weather?longitud=${longitud}&latitud=${latitud}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('string');
+                    res.body.should.be.equal('ERR: invalid coordinate');
+                    done();
+                 });
          });
 
-         */
+         // Test to get single student record
+        it("should get error of product", (done) => {
+            const product = 'architect';
+            chai.request(app)
+                .get(`/apiClient/weather?product=${product}`)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('ERR');
+                    done();
+                 });
+         });
+
+         // Test to get single student record
+         it("should get error of output", (done) => {
+            const output = 'csv';
+            chai.request(app)
+                .get(`/apiClient/weather?output=${output}`)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('ERR');
+                    done();
+                 });
+         });
+
     });
 });
